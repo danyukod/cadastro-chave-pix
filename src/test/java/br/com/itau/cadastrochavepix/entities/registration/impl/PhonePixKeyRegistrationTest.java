7793b5e1dec7319291e86caf6fe98f4f83b25228
Danilo Kodavara <danilo.kodavara@hotmail.com>
2023-01-27 22:13:20 -0300

    feat: add jacoco coverage, e criando testes para validacao de formato de telefone.

--- src/test/java/br/com/itau/cadastrochavepix/entities/registration/impl/PhonePixKeyRegistrationTest.java
+++ src/test/java/br/com/itau/cadastrochavepix/entities/registration/impl/PhonePixKeyRegistrationTest.java
@@ -1,13 +1,42 @@
 package br.com.itau.cadastrochavepix.entities.registration.impl;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PhonePixKeyRegistrationTest {
 
+    PhonePixKeyRegistration phonePixKeyRegistration;
+
+    @BeforeEach
+    void setUp(){
+        phonePixKeyRegistration = new PhonePixKeyRegistration();
+    }
+
     @Test
     void givenAValidPhoneNumberShouldReturnTrue() {
+        String phoneNumber = "+5515999999999";
+        boolean result = phonePixKeyRegistration.formatValidate(phoneNumber);
 
-
-
+        assertTrue(result);
     }
+
+    @Test
+    void givenAInvalidPhoneNumberShouldReturnFalse() {
+        String phoneNumberWithoutPlus = "5515999999999";
+        String phoneNumberLessDigits = "+551599999999";
+        String phoneNumberHighestDigits = "+551599999999";
+        boolean resultWithoutPlus = phonePixKeyRegistration.formatValidate(phoneNumberWithoutPlus);
+        boolean resultLessDigits = phonePixKeyRegistration.formatValidate(phoneNumberLessDigits);
+        boolean resultHighestDigits = phonePixKeyRegistration.formatValidate(phoneNumberHighestDigits);
+
+
+        assertFalse(resultWithoutPlus);
+        assertFalse(resultLessDigits);
+        assertFalse(resultHighestDigits);
+    }
+
+
 }
