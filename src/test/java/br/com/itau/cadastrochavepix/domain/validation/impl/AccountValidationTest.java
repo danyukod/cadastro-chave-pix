c25d2280bb102e338bc7f0c141e81101007898e7
Danilo Kodavara <danilo.kodavara@hotmail.com>
2023-02-03 23:31:38 -0300

    feat: finalizando testes de validacao de conta.

--- src/test/java/br/com/itau/cadastrochavepix/domain/validation/impl/AccountValidationTest.java
+++ src/test/java/br/com/itau/cadastrochavepix/domain/validation/impl/AccountValidationTest.java
@@ -3,8 +3,10 @@ package br.com.itau.cadastrochavepix.domain.validation.impl;
 import br.com.itau.cadastrochavepix.model.requests.PixKeyRegisterRequest;
 import br.com.itau.cadastrochavepix.model.requests.PixKeyRequest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AccountValidationTest {
@@ -18,11 +20,26 @@ class AccountValidationTest {
     }
 
     @Test
+    @DisplayName("Should return true when given valid account")
     void shouldReturnTrueWhenGivenValidAccout() {
         PixKeyRequest pixKeyRequestWithValidAccount = createPixKeyRequest("CORRENTE",
                 1234, 12345678);
 
         assertTrue(accountValidation.accountValidate(pixKeyRequestWithValidAccount));
+    }
+
+    @Test
+    void shouldReturnFalseWhenGivenInvalidAccout() {
+        PixKeyRequest pixKeyRequestWithInvalidAccountType = createPixKeyRequest("TESTE",
+                1234, 12345678);
+        PixKeyRequest pixKeyRequestWithInvalidAgencyNumber = createPixKeyRequest("CORRENTE",
+                12345, 12345678);
+        PixKeyRequest pixKeyRequestWithInvalidAccountNumber = createPixKeyRequest("CORRENTE",
+                1234, 123456789);
+
+        assertFalse(accountValidation.accountValidate(pixKeyRequestWithInvalidAccountType));
+        assertFalse(accountValidation.accountValidate(pixKeyRequestWithInvalidAgencyNumber));
+        assertFalse(accountValidation.accountValidate(pixKeyRequestWithInvalidAccountNumber));
     }
 
     private PixKeyRequest createPixKeyRequest(String accountType, Integer agencyNumber, Integer accountNumber) {
